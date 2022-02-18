495ed436b171a2f45c7ac0abb2a302838f133c2b
ruspet <74701088+ruspet@users.noreply.github.com>
2022-02-18 19:30:37 +0300

    Laba1
    Первая версия

---
+++ Лабораторная_работа_1/Example_1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System; //включение пространства имен
+using System.Collections.Generic; //включение пространства имен
+using System.Linq; //включение пространства имен
+using System.Text; //включение пространства имен
+using System.Threading.Tasks; //включение пространства имен
+
+namespace Example_1 //пространство имен Example_1
+{
+    class Program //класс Program
+    {
+        static void Main(string[] args) //главная функция(точка входа в программу)
+        {
+            Console.WriteLine("Hello, world!"); //вывод строки
+            Console.ReadLine(); //ожидание нажатия клавиши
+            string name;
+            Console.WriteLine("Введите имя: ");
+            name = Console.ReadLine();
+            string secondName;
+            Console.WriteLine("Введите фамилию: ");
+            secondName = Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine($"Здравствуй, { name} { secondName}!");
+
+            string sAge;
+            byte age;
+            Console.WriteLine("Введите возраст: ");
+            sAge = Console.ReadLine();
+            if (byte.TryParse(sAge, out age))
+            {
+                if (age < 18)
+                {
+                    Console.WriteLine("Несовершеннолетний");
+                }
+                else if (age > 100)
+                    Console.WriteLine("Столько не живут");
+                else
+                    Console.WriteLine("Совершеннолетний");
+            }
+            else
+                Console.WriteLine("Ошибка ввода");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Лабораторная_работа_1/Example_2/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string textMenu;
+            int res;
+            
+            DateTime d;
+            textMenu = @" Выберите действие: 
+            1 - Вывести текущую дату;
+            2 - Вывести текущее время;
+            3 - Выход из программы";
+            d = DateTime.Now;            
+            
+                Console.Clear();
+                Console.WriteLine(textMenu);
+                res = Convert.ToInt32(Console.ReadLine());
+
+                switch (res)
+                {
+                    case 1:
+                        {
+                            Console.WriteLine("Сегодня: {0}", d.ToShortDateString());
+                            break;
+                        }
+                    case 2:
+                        {
+                            Console.WriteLine("Сейчас: {0}", d.ToShortTimeString());
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Выход из программы");
+                            break;
+                        }
+                    default:
+                        {
+                            Console.WriteLine("Ошибка ввода"); break;
+                        }                   
+                }
+                 Console.WriteLine();
+            
+        }
+    }
+}

4834d13681f748ddef3e30184eb3eea1fd97f21a
ruspet <74701088+ruspet@users.noreply.github.com>
2022-02-24 15:28:09 +0300

    ExamCSharp

---
+++ ExamCSharp/ExamCSharp/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Management;
+
+namespace Comp_Info
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Версия вашей ОС: {0}", Environment.OSVersion);
+            Console.WriteLine("Эта ОС 64-х битная? {0}", Environment.Is64BitOperatingSystem ? "Да" : "Нет");
+            Console.WriteLine("Имя компьюера: {0}", Environment.MachineName);
+            Console.WriteLine("Логические диски: {0}", String.Join(", ", Environment.GetLogicalDrives())); //.TrimEnd(',', ' ').Replace("\\", String.Empty));
+            Console.WriteLine("Количество процессоров: {0}", Environment.ProcessorCount);
+            Console.WriteLine("Системная папка: {0}", Environment.SystemDirectory);
+            
+            Console.ReadLine();
+        }
+    }
+}
